5b29da2a1c726defa579a538cfd71427fcea17d6
RicardoUy <rvuy62@yahoo.com>
2024-03-24 20:57:16 -0500

    add shop cat, shop context

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,9 +1,15 @@
-import React from "react";
-import { createContext } from "react";
 
+import React, { createContext } from "react";
+import all_product from "../components/Assets/Assets/all_product"
 export const ShopContext = createContext(null);
 
 const ShopContextProvider = (props) => {
 
-    const contextValue = {}
+    const contextValue = {all_product}
+    return(
+        <ShopContextProvider value={contextValue}>
+            {props.children}
+        </ShopContextProvider>
+    )
 }
+export default ShopContextProvider;
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Footer.css'
+import footer_logo from '../Assets/Assets/logo_big.png'
+import instagram_icon from '../Assets/Assets/instagram_icon.png'
+import pintester_icon from '../Assets/Assets/pintester_icon.png'
+import whatsapp_icon from '../Assets/Assets/whatsapp_icon.png'
 const Footer = () => {
   return (
     <div className='footer'>
     <div className="footer-logo">
-        <img src='' alt="" />
+        <img src={footer_logo} alt="" />
         <p>SHOPPER</p>
     </div>
     <ul className='"footer-links'>
@@ -16,13 +20,13 @@ const Footer = () => {
     </ul>
 <div className="footer-social-icon">
     <div className="footer-icons-container">
-        <img src="" alt="" />
+        <img src={instagram_icon} alt="" />
     </div>
     <div className="footer-icons-container">
-        <img src="" alt="" />
+        <img src={pintester_icon} alt="" />
     </div>
     <div className="footer-icons-container">
-        <img src="" alt="" />
+        <img src={whatsapp_icon} alt="" />
     </div>
 </div>
 <div className="footer-copyright">
